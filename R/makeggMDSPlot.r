c12592654b55e0f41460ad0473a554f973f8df60
Vsevolod Makeev <vsevolodmakeev@vsevolods-air.home>
2024-10-28 22:03:27 +0000

    MDSplot 3

--- R/makeggMDSPlot.r
+++ R/makeggMDSPlot.r
@@ -6,10 +6,12 @@ makeggMDSPlot <- function(DGEListObject){
 	wd		<- getwd()
 	plotDirName	<- file.path(wd, "Plot")
 	plotDir		<- dir.create(plotDirName, showWarnings = FALSE)
-	MDSPlotFile	<- file.path(plotDir, paste0("MDSPLot", deparse(substitute(DGEListObject)), "pdf"))
+	MDSPlotFile	<- file.path(plotDirName, paste0("MDSPLot",".png"))
 	
 	pMDS	 	<- plotMDS(cpm(DGEListObject))
 	pMDSData 	<- data.frame( pMDS$x, pMDS$y, DGEListObject$samples$group)
 	colnames(pMDSData) <- c("x", "y", "dataset")
-	makeggMDSPlot	<- ggplot( mdsCpmPlotData, aes( x = x, y = y, color = dataset))+geom_point()
+	ggMDSPlot	<- ggplot( mdsCpmPlotData, aes( x = x, y = y, color = dataset))+geom_point()
+
+	ggsave(filename = MDSPlotFile, device = 'png', dpi = 300)
 }

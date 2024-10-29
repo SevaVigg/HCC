32ef702d06429562d164c6cd5f0a5682f47c5533
Vsevolod Makeev <vsevolodmakeev@Vsevolods-MacBook-Air.local>
2024-10-29 12:22:11 +0000

    debug plotMDS

--- R/makeggMDSPlot.r
+++ R/makeggMDSPlot.r
@@ -1,4 +1,4 @@
-makeggMDSPlot <- function(DGEListObject){
+makeggMDSPlot <- function(DGEListObject, plotName = "DGEList"){
 	library(tidyverse)
 	library(edgeR)
 
@@ -6,12 +6,12 @@ makeggMDSPlot <- function(DGEListObject){
 	wd		<- getwd()
 	plotDirName	<- file.path(wd, "Plot")
 	plotDir		<- dir.create(plotDirName, showWarnings = FALSE)
-	MDSPlotFile	<- file.path(plotDirName, paste0("MDSPLot",".png"))
+	MDSPlotFile	<- file.path(plotDirName, paste0("MDSPLot_",plotName, ".png"))
 	
 	pMDS	 	<- plotMDS(cpm(DGEListObject))
 	pMDSData 	<- data.frame( pMDS$x, pMDS$y, DGEListObject$samples$group)
 	colnames(pMDSData) <- c("x", "y", "dataset")
-	ggMDSPlot	<- ggplot( mdsCpmPlotData, aes( x = x, y = y, color = dataset))+geom_point()
+	ggMDSPlot	<- ggplot( pMDSData, aes( x = x, y = y, color = dataset))+geom_point() + ggtitle( plotName)
 
 	ggsave(filename = MDSPlotFile, device = 'png', dpi = 300)
 }

6b05a5b99c1b9434bbdf20ed61614ac850118a0e
SevaVigg <vsevolodmakeev@Vsevolods-MacBook-Air.local>
2025-07-06 15:24:20 +0100

    TCGA names

--- R/processBulkHCC.r
+++ R/processBulkHCC.r
@@ -1,4 +1,5 @@
 require("useful")
+require("tidyverse")
 require("edgeR")
 
 wd <- getwd()
@@ -7,6 +8,28 @@ DataDir <- dir.create(file.path(wd, "Data") , showWarnings = FALSE)
 
 load("Data/Dec_data.RData")
 
-hccPCA <- princomp(Total.counts)
+Total.counts 	%>% rownames_to_column(var = "gene") %>% 
+			pivot_longer( cols = -gene, names_to = "original_column_name", values_to = "expression") -> 
+			longTotalCounts
+
+longTotalCounts %>% filter(str_detect(original_column_name, "TCGA") %>%
+			separate_wider_delim(cols = original_column_name, delim = ".", 
+			names = c(	"Database", 
+					"Tissue sourse", 
+					"Patient", 
+					"Sample type and vial", 
+					"Portion and analyte", 
+					"Plate", 
+					"Center"), too_few = "align_start", too_many = "merge") -> longTotalCount
+
+
+longTotalCounts %>% filter(str_detect(original_column_name, "Sample") %>%
+			separate_wider_delim(cols = original_column_name, delim = "_", 
+			names = c(	"Database", 
+					"Patient_TissueSource_Replicate"),
+						   too_few = "align_start", too_many = "merge") -> longTotalCount
+
+
+#hccPCA <- princomp(Total.counts)
 
 
